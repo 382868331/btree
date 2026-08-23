e52c0111c478735ce73d8afb3f6655a79f4bef65
382868331 <382868331@users.noreply.github.com>
2026-08-24 04:23:35 +0800

    test: reproduce 树长度计数

---
+++ goletalab_019_test.go
@@ -0,0 +1,8 @@
+package btree
+
+import ("fmt";"testing")
+
+var _ = fmt.Sprint
+func taskTree013(v ...int)*BTree{t:=New(2);for _,n:=range v{t.ReplaceOrInsert(Int(n))};return t}
+func collect013(run func(ItemIterator))[]int{var out []int;run(func(v Item)bool{out=append(out,int(v.(Int)));return true});return out}
+func TestGoletaBTree019(t *testing.T){tr:=taskTree013(2,4,6);if tr.Len()!=3{t.Fatalf("len=%d",tr.Len())}}
