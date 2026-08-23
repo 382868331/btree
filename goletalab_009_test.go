a0de08fcb45b8e7ea3ccb05029e4ccf3ad98df0a
382868331 <382868331@users.noreply.github.com>
2026-08-24 04:16:14 +0800

    test: reproduce 成员判断布尔值

---
+++ goletalab_009_test.go
@@ -0,0 +1,8 @@
+package btree
+
+import ("fmt";"testing")
+
+var _ = fmt.Sprint
+func taskTree013(v ...int)*BTree{t:=New(2);for _,n:=range v{t.ReplaceOrInsert(Int(n))};return t}
+func collect013(run func(ItemIterator))[]int{var out []int;run(func(v Item)bool{out=append(out,int(v.(Int)));return true});return out}
+func TestGoletaBTree009(t *testing.T){tr:=taskTree013(1,3);if !tr.Has(Int(3))||tr.Has(Int(2)){t.Fatalf("present=%v absent=%v",tr.Has(Int(3)),tr.Has(Int(2)))}}
