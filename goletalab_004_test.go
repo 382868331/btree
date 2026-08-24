553e08bc5f835548fd49bf1ce288c97bc1df092f
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:48:51 +0800

    fix: restore 非法degree校验缺失

--- goletalab_004_test.go
+++ goletalab_004_test.go
@@ -2,3 +2,10 @@ package btree
 import("fmt";"testing")
 var _=fmt.Sprint
 func TestGoletaBTree004(t *testing.T){func(){defer func(){if recover()==nil{t.Fatal("degree 1 accepted")}}();NewOrderedG[int](1)}()}
+
+func TestGoletaBTree004Boundary(t *testing.T){
+ func(){
+ defer func(){if recover()==nil{t.Fatal("zero degree accepted")}}()
+ NewOrderedG[int](0)
+}()
+}
