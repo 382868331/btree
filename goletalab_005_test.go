f6bfba8c454b97c5845df2bc5cb912b2fccab42a
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:49:04 +0800

    fix: restore 首次插入长度漏记

--- goletalab_005_test.go
+++ goletalab_005_test.go
@@ -2,3 +2,9 @@ package btree
 import("fmt";"testing")
 var _=fmt.Sprint
 func TestGoletaBTree005(t *testing.T){tr:=NewOrderedG[int](3);tr.ReplaceOrInsert(8);if !tr.Has(8)||tr.Len()!=1{t.Fatalf("has=%v len=%d",tr.Has(8),tr.Len())}}
+
+func TestGoletaBTree005Boundary(t *testing.T){
+ tr:=NewOrderedG[int](2)
+ tr.ReplaceOrInsert(-1);tr.ReplaceOrInsert(4)
+ if tr.Len()!=2{t.Fatalf("len=%d",tr.Len())}
+}
