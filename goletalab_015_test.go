da34ee3cdfb575743fb1473c3b567a8b082ccbc3
382868331 <382868331@users.noreply.github.com>
2026-08-24 15:35:35 +0800

    fix: restore 分裂子节点插入槽错误

--- goletalab_015_test.go
+++ goletalab_015_test.go
@@ -4,3 +4,9 @@ import ("fmt";"testing")
 
 var _ = fmt.Sprint
 func TestGoletaBTree015(t *testing.T){tr:=NewOrderedG[int](2);for _,v:=range []int{0,-8,8,-4,4,-6,-2,2,6,-7,-5,-3,-1,1,3,5,7}{tr.ReplaceOrInsert(v)};for v:=-8;v<=8;v++{if !tr.Has(v){t.Fatalf("missing %d",v)}}}
+
+func TestGoletaBTree015Boundary(t *testing.T) {
+ tr:=NewOrderedG[int](3)
+ for i:=50;i>=1;i--{tr.ReplaceOrInsert(i)}
+ if _,ok:=tr.Get(37);!ok{t.Fatal("37 not reachable")}
+}
